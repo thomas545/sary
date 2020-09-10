f3274f9420cae637e56c43538f5c01a7c984e7dc
thomas434 <thomas@dokkanz.com>
2020-09-10 16:07:54 +0200

    add docs for permissions

--- task/permissions.py
+++ task/permissions.py
@@ -6,6 +6,11 @@ from .models import Task
 
 
 class StateNotDone(BasePermission):
+    """
+    Edit if state is New
+    Custom permission for state if isn't New anyone can't update title & description
+    """
+
     def has_object_permission(self, request, view, obj):
         if obj.state in [Task.INPROGRESS, Task.DONE]:
             raise PermissionDenied("Can't edit task, state is done.")
@@ -13,6 +18,11 @@ class StateNotDone(BasePermission):
 
 
 class StateInProgress(BasePermission):
+    """
+    Edit or linked tasks if state is In Progress
+    Custom permission for state if isn't In Progress anyone can't link tasks together
+    """
+
     def has_object_permission(self, request, view, obj):
         if not can_proceed(obj.in_progress) or not can_proceed(
             obj.linked_task.in_progress
